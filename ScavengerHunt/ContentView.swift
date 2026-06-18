f2853e460a81b9b3bcceabb2542834fdb668b8c8
Nigel-Kallan <nigel.kallan@triosstudent.com>
2026-06-18 16:30:58 -0400

    Initial Commit

---
+++ ScavengerHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengerHunt
+//
+//  Created by NIGEL KALLAN on 2026-06-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by NIGEL KALLAN on 2026-06-18.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
